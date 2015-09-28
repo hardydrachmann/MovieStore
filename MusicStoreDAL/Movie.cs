d504e8bde072ae5bcede1eca9a2a26ca84b451c3
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2015-09-28 10:41:16 +0200

    Some stuff added

---
+++ MusicStoreDAL/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreDAL
+{
+    class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public int Year { get; set; }
+        public string ImageURL { get; set; }
+        public string TrailerURL { get; set; }
+        public string Genre { get; set; }
+    }
+}
