f4db8efb8bb73432bff31d47e718857192bd2730
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2015-10-14 14:10:53 +0200

    added CustomerUI

---
+++ MovieStoreCustomerUI/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using MovieStoreDAL;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+public class OrderController : Controller
+{
+    private CustomerRepository cusRepo = new CustomerRepository();
+    private OrderRepository ordRepo = new OrderRepository();
+
+    // GET: Order
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    // Order List
+    public ActionResult OrderList(string eMail)
+    {
+        IEnumerable<Customer> customers = cusRepo.GetAll();
+
+        foreach (var customer in customers)
+        {
+            if (eMail == customer.Email)
+            {
+                return View(customer);
+            }
+        }
+        return RedirectToAction("index");
+    }
+}
