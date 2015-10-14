780483795f20f2b7982d83d12375b997a0f84f0c
Rasmus Nielsen <rasmniel@gmail.com>
2015-10-14 11:03:46 +0200

    MovieController added

---
+++ MovieStoreCustomerUI/Controllers/MovieController.cs
@@ -0,0 +1,105 @@
+﻿using MovieStoreDAL;
+using System.Net;
+using System.Web.Mvc;
+
+namespace MovieStoreAdminUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        private MovieRepository repo = new MovieRepository();
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View(repo.GetAll());
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int id)
+        {
+            Movie movie = repo.Get(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // GET: Movie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movie/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Add(movie);
+                return RedirectToAction("Index");
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movie/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Movie movie = repo.Get(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movie/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Edit(movie);                
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Movie movie = repo.Get(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {           
+            repo.Remove(id);            
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                repo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
