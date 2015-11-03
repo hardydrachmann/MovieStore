8a169fdb13633c331486334e90e5e6c38bb2027a
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2015-11-03 09:56:35 +0100

    tests added

---
+++ MovieStoreTests/FakeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MovieStoreTests
+{
+    [TestFixture]
+    public class FakeTests
+    {
+        [Test]
+        public void FakeTest()
+        {
+            int numberOne = 1;
+            int numberTwo = 2;
+
+            Assert.AreNotEqual(numberOne, numberTwo);
+        }
+    }
+}
