f5efe78a25cea8815cfbceca6d370d7611ee2423
Rasmus Nielsen <rasmniel@gmail.com>
2015-11-06 10:56:02 +0100

    CreateMovieViewModel

---
+++ MovieStoreAdminUI/Models/CreateMovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MovieStoreDAL;
+using MovieStoreDAL.DomainModels;
+using System.Collections.Generic;
+
+namespace MovieStoreAdminUI.Models
+{
+    public class CreateMovieViewModel
+    {
+        public Movie Movie { get; set; }
+
+        public IEnumerable<Genre> AllGenres { get; set; }
+    }
+}
