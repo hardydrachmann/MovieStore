d186b09dc37324e90b788ef80e2bac5f790943e8
Rasmus Nielsen <rasmniel@gmail.com>
2015-11-11 11:05:38 +0100

    Added BE layer for real this time

---
+++ BE/DomainModels/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieStoreBE
+{
+    public class Movie
+    {
+        [Display(Name = "Movie Number")]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [Display(Name = "Price (Dkk)")]
+        public double Price { get; set; }
+
+        [Display(Name = "Release Year")]
+        public int Year { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageURL { get; set; }
+
+        [Display(Name = "Trailer")]
+        public string TrailerURL { get; set; }
+
+        public virtual List<Genre> Genres { get; set; }
+    }
+}
---
+++ BE/DomainModels/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieStoreBE
+{
+    public class Order
+    {
+        [Display(Name = "Order Number")]
+        public int Id { get; set; }
+
+        public string Date { get; set; }
+
+        [Display(Name= "Movie Number")]
+        public int MovieId { get; set; }
+
+        public virtual Movie Movie { get; set; }
+
+        [Display(Name= "Customer Number")]
+        public int CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+}
